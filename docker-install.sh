2913f07e8077a68c5b3b0c789536541a1cb68548
shashikanth <Shashikanth0312@gmail.com>
2026-08-21 16:44:46 +0530

    creating backend image file

---
+++ docker-install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+sudo dnf remove docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-engine \
+                  podman \
+                  runc
+
+echo "removed the previous docker files"
+
+sudo dnf -y install dnf-plugins-core
+
+sudo dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+
+echo "installing docker...."
+sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+echo "installed docker successfully"
+
+sudo systemctl enable docker
+
+sudo systemctl start docker
