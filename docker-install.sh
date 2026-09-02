892c862e5ac3d9128af517dc993f9c55d5f3fb25
Shashikanth Kammari <shashikanth0312@gmail.com>
2026-09-02 22:36:46 +0530

    adding

--- docker-install.sh
+++ docker-install.sh
@@ -16,6 +16,8 @@ sudo dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker
 
 sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
+echo "Docker starting..."
+
 sudo systemctl start docker
 
 sudo systemctl enable docker
